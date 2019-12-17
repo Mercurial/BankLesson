bf5ce48f202de369e234724c7486eeb523f04574
Clark Alesna <clark_alesna@hotmail.com>
2019-12-18 01:12:22 +0800

    Base Code

---
+++ Bank.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Bank {
+    public string Name { get; set; }
+    public Account[] Accounts { get; private set; }
+
+    private int TotalAccounts { get; set; } = 0;
+
+    public Bank(string name)
+    {
+        Name = name;
+        Accounts = new Account[10];
+        for(var x = 0; x < Accounts.Length; x++) Accounts[x] = new Account();
+    }
+
+    public string GetWelcomeMessage()
+    {
+        return $"Welcome to {Name}";
+    }
+
+    public Guid Register(string username, string password)
+    {
+        Accounts[TotalAccounts].Username = username;
+        Accounts[TotalAccounts].Password = password;
+        TotalAccounts++;
+
+        return Accounts[TotalAccounts].Id;
+    }
+
+    public Account Login(string username, string password)
+    {
+        foreach(Account a in Accounts)
+        {
+            if(a.Username == username && a.Password == password)
+            {
+                return a;
+            }
+        }
+
+        return null;
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ClarkBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bank = new Bank("Union Bank");
+
+            string username = string.Empty;
+            string password = string.Empty;
+            bool shouldContinue = true;
+            while (shouldContinue)
+            {
+                Console.Clear();
+                Console.WriteLine(bank.GetWelcomeMessage());
+                Console.WriteLine("Enter 1 to Login, Enter 2 to Register");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("[LOGIN]");
+                        Console.WriteLine("Enter Username: ");
+                        username = Console.ReadLine();
+                        Console.WriteLine("Enter Password: ");
+                        password = Console.ReadLine();
+                        var account = bank.Login(username, password);
+                        if (account != null)
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"Welcome back, {account.Username}");
+                            Console.WriteLine($"Account Id, {account.Id}");
+                            Console.WriteLine($"Your balance is: ${account.Balance}");
+                            Console.ReadKey();
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Username or Password, press any key to continue..");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("[REGISTER]");
+                        Console.WriteLine("Enter Username: ");
+                        username = Console.ReadLine();
+                        Console.WriteLine("Enter Password: ");
+                        password = Console.ReadLine();
+                        var id = bank.Register(username, password);
+                        Console.WriteLine($"Congratulations, you have succesfully registered! {id}");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid Option, press any key to continue...");
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
